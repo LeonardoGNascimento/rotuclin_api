72c9585a68f3f562d9442229bdf25d8ea4fd003c
LeonardoGNascimento <leonardogabrielllpedroso@gmail.com>
2025-07-05 09:42:00 -0300

    logo

--- admin/components/Logo.tsx
+++ admin/components/Logo.tsx
@@ -2,11 +2,10 @@ export function Logo() {
   return (
     <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
       <img
-        src="/logo.png" // coloque sua logo em public/logo.svg
+        src="http://rotuclin-minio-bf124c-69-62-86-212.traefik.me:9000/public/rotuclin/logo.png" // coloque sua logo em public/logo.svg
         alt="Minha Logo"
         style={{ height: "2rem" }}
       />
-      <strong style={{ fontSize: "1.2rem" }}>Minha Aplicação</strong>
     </div>
   );
 }
