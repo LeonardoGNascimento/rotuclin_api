07389486ebc28c1162303a42a20aade000397269
Caio Labella <caiolabella2016@gmail.com>
2025-08-03 11:32:44 -0300

    atualizando endpoint do Minio e ajustando URLs da logo para HTTPS

--- admin/components/Logo.tsx
+++ admin/components/Logo.tsx
@@ -2,7 +2,7 @@ export function Logo() {
   return (
     <div style={{ display: "flex", alignItems: "center", gap: "0.5rem" }}>
       <img
-        src="http://rotuclin-minio-bf124c-69-62-86-212.traefik.me:9000/public/rotuclin/logo.png" // coloque sua logo em public/logo.svg
+        src="https://minio.rotuclin.com.br/public/rotuclin/logo.png" // coloque sua logo em public/logo.svg
         alt="Minha Logo"
         style={{ height: "2rem" }}
       />
